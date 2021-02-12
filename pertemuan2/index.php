5880db2a356ad1ff629a31e91f6ca1a8aa03edf6
sacred142 <agung.alfatah43@gmail.com>
2021-02-12 22:02:32 +0700

    menyelesaikan pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,68 @@
+<?php
+/*
+agung alfatah
+203040171
+https://github.com/sacred142/pw2021_203040171
+Pertemuan 2 - 12 februari 2021
+belajar mengenai sintax PHP
+*/
+?>
+
+<?php
+//standart output
+//echo,print
+//print_r
+//var_dump
+
+//penulisan sintax php
+//1.php dalam html
+//2.html dalam php
+
+
+//variabel dan tipe data
+//tidak boleh diawali angka tapi boleh mengandung angka
+//$nama = "Agung";
+//echo "nama saya $nama";
+//echo 'nama saya $nama';
+
+
+//operator
+//aritmatika
+//+ - * : %
+//echo 1 + 1
+//$x = 10;
+//$y = 20;
+//echo $x * $y;
+
+
+//penggabung string/concatenation/concat
+//.
+//$nama_depan : "agung";
+//$nama_belakang :"alfatah";
+//echo $nama_depan . " ". $nama_belakang
+
+//assignment
+//=, +=, -+, *=, %=, .=
+//$x = 1;
+//$x -= 5;
+//echo $x;
+//$nama = "agung";
+//$nama .= " ";
+//$nama .= "alfatah";
+//echo $nama
+
+
+//perbandingan
+//<,>,<=,>=,==,!=
+//var_dump(1 =="1");
+
+//identitas
+//===, !==
+//var_dump(1 ==="1");
+
+//logika
+//&&, ||,!
+
+$x = 30;
+var_dump($x < 20 || $x % 2 ==0);
+?>
