0a1c161d71708037d88c2348db0d0832a639cba4
sacred142 <agung.alfatah43@gmail.com>
2021-03-05 03:09:22 +0700

    tugas pertemuan 4

---
+++ pertemuan4/date.php
@@ -0,0 +1,49 @@
+<?php
+/*
+agung alfatah
+203040171
+https://github.com/sacred142/pw2021_203040171
+Pertemuan 4 - 5 maret 2021
+belajar mengenai function date time mk time dan strtotime
+*/
+?>
+
+<?php 
+//date
+//untuk menampilkan tanggal dengan format tertentu
+// echo date("l,d-M-Y");
+
+//time
+//UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 januari 1970
+// echo time();
+// echo date("l d M Y", time()-60*60*24*100);
+
+
+//mktime
+//membuat sendiri detik
+//mktime(0,0,0,0,0,0,)
+//jam,menit,detik,bulan,tanggal,tahun
+
+// echo date("l",mktime(0,0,0,8,25,1985));
+
+//strtotime
+// echo date("l",strtotime("25 aug 1985"));
+
+//function yang akan bnyk digunakan di seri php
+//strlen() untuk menghitung panjang sebuah string
+//strcmp() untuk membandingkan string
+//explode() memecah string menjadi array
+//htmlspecialchars()
+
+//utility
+//var_dump()
+//isset() 
+//empty()
+//die()
+//sleep()
+
+
+//user defined Function
+
+?>
---
+++ pertemuan4/function.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+agung alfatah
+203040171
+https://github.com/sacred142/pw2021_203040171
+Pertemuan 4 - 5 maret 2021
+belajar mengenai membuat function kita sendiri
+*/
+?>
+
+
+<?php 
+function salam($waktu = "Datang", $nama = "Admin") {
+    return "Selamat $waktu, $nama!";
+}
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan function</title>
+</head>
+<body>
+    <h1><?php echo salam("pagi", "Agung") ?></h1>
+</body>
+</html>
